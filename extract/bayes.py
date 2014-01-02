c0319fa832cdd458ace34d87ddc5e3aef11f67cf
JKN <yanghf1985@hotmail.com>
2014-01-02 13:43:46 +0800

    添加注释

--- extract/bayes.py
+++ extract/bayes.py
@@ -2,11 +2,18 @@
 import codecs
 from copy import deepcopy
 
+# 使用朴素贝叶斯算法在商品名称中提取品牌,
+# 定理参见: http://zh.wikipedia.org/wiki/贝叶斯定理
+# 思路:
+#    将品牌名看作是有由<起点a,终点b>组成
+#    使用样本生成字典, 分别计算给定字符串中每一个样本字符后面是起点,终点的概率, 取使概率最大的那个字符
+#    通过a,b截取品牌名
 class Naive(object):
     
     def __init__(self,debug = True):
         self.debug = debug
         
+    # "起点"统计, "终点"统计
     def traning(self,samples):
         self.tab = self.calc_init(samples)
         if self.debug:
@@ -18,7 +25,7 @@ class Naive(object):
         if self.debug:
             self.debug_print(self.end)
         
-        
+    # 根据训练样本提取给定字符串的商品名    
     def extract(self,str):
         if self.begin and self.end:
             a,aprob = self.argmax_pi_a(self.begin,str)
@@ -29,7 +36,7 @@ class Naive(object):
         else:
             raise Exception("has not yet trained")    
        
-         
+    # 初始化计数表     
     def calc_init(self,path):
         symbols = {}
         symbols['^'] = [0,0,0]
@@ -56,14 +63,15 @@ class Naive(object):
                     "map":symbols
                  }
                }
-
+            
+    # 调试输出 统计表
     def debug_print(self,tab):
         print "summary:samples=%d,chars=%d" % (tab['summary']['samples'],tab['symbol']['total'])
         print "------------------------------------"
         for k,sym in tab['symbol']['map'].items():
             print "%s=%r" % (k.encode('utf-8'),sym)
             
-    #    
+    # 统计样本中字符前(后) 出现(品牌名), 不出现(品牌) 的次数  
     def _count(self,path,samples,start=1):
         symbols = deepcopy(samples)
         f = codecs.open(path,'r','utf-8')
@@ -94,7 +102,8 @@ class Naive(object):
         f.close()
         return symbols
     
-    
+    # 出现品牌名的概率
+    # 实际应用中这个不需要, 每个分母都同除这个参数, 具体值不影响结果
     def pi(self,samples):
         n = 0
         total = samples['summary']['samples']
@@ -102,7 +111,7 @@ class Naive(object):
             n += v[0]
         return float(n)/total
         
-    # prob of the given char        
+    # 出现给定字符的概率        
     def pa(self,samples,ch):
         total = samples['summary']['samples']
         tmp = samples['symbol']['map'][ch]
@@ -111,14 +120,14 @@ class Naive(object):
             print "pa is 0: %s" % ch
         return value
 
-    # prob of the given position
+    # 出现品牌名并且包含给定字符的概率
     def pa_i(self,samples,ch):
         _sum = samples['summary']['samples'] #每一个样本只有一个"开始位置" >> total = len(samples)
         if _sum == 0:
             return 0
         return float(samples['symbol']['map'][ch][0])/_sum
    
-    # prob of position i which on a given char 
+    # 给定字符出现品牌名的概率 
     def pi_a(self,samples,ch):
         _pi = self.pi(samples)
         if _pi == 0:
@@ -130,7 +139,7 @@ class Naive(object):
             print "pi_a = %.3f (ch=%s,pa_i=%.3f,pa=%.3f,pi=%.3f)" % (value,ch.encode('utf-8'),_pa_i,_pa,_pi)
         return value
     
-    # 
+    # 计算使pi_a最大的字符
     def argmax_pi_a(self,samples,line):
         maxp = 0
         i = 0

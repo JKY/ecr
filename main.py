6668d3847622279cd2090c8ba25765086761413e
JKN <yanghf1985@hotmail.com>
2014-01-03 11:20:25 +0800

    添加标注数据
    测试准确率为40%

--- main.py
+++ main.py
@@ -18,16 +18,28 @@ def load_test(path):
            
 
 if __name__ == '__main__':   
+    s = u'友阳yosun不锈钢落地可伸缩收纳X型晾衣晒被架Y-6303'
     extr = NaiveBayesExtractor()
     extr.traning('./data/brand_sample.txt')
-    str = u'sdsdsdafa'
-    lines = load_test('./data/brand_test.txt')
-    sum = len(lines)
-    error = 0
-    for str,entry in lines:
-        begin,end = extr.extract(str)
-        result = str[begin:end-1]
-        print "品牌:%s" % result.encode('utf-8')
-        if entry != result:
-            error += 1
-    print "total:%d,error=%d,er=%f" % (sum,error,float(error)/sum)
+    begin,end = extr.extract(s)
+    result = s[begin:end-1]
+    print "品牌:%s" % result.encode('utf-8')
+            
+    
+    #extr = NaiveBayesExtractor()
+    #extr.traning('./data/brand_sample.txt')
+    #str = u'sdsdsdafa'
+    #lines = load_test('./data/brand_test.txt')
+    #sum = len(lines)
+    #error = 0
+    #error_f = codecs.open("./data/err.txt","w","utf-8")
+    #for str,entry in lines:
+    #    begin,end = extr.extract(str)
+    #    result = str[begin:end-1]
+    #    print "品牌:%s" % result.encode('utf-8')
+    #    if entry != result:
+    #        error += 1
+    #        error_f.write(str + "\n")
+    #error_f.close()
+    #print "total:%d,error=%d,er=%f" % (sum,error,float(error)/sum)
+    
